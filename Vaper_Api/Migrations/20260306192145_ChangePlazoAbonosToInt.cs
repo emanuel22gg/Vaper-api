ff70b2fdc30bfa6dbe55be8648e45120c22803fe
juanvelasquez24 <juanvelasquez2@jaga.edu.co>
2026-03-06 15:50:13 -0500

    ultimo-cambio

---
+++ Vaper_Api/Migrations/20260306192145_ChangePlazoAbonosToInt.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vaper_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePlazoAbonosToInt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PlazoAbonos",
+                table: "Venta_Pedidos",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldUnicode: false,
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PlazoAbonos",
+                table: "Venta_Pedidos",
+                type: "varchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
